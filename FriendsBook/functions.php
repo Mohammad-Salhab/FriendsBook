9e8ae4d0a5d70fff4184f12b564708022485ccf6
Mohammad-Salhab <181123@ppu.edu.ps>
2022-05-19 15:01:56 +0200

    final-update

--- FriendsBook/functions.php
+++ FriendsBook/functions.php
@@ -2,14 +2,20 @@
   session_start();
 
   function db_connect() {
+    
+    if (!function_exists('mysqli_init') && !extension_loaded('mysqli')) {
+      echo 'We don\'t have mysqli!!!';
+  } else {
+      echo 'Phew we have it!';
+  }
     global $conn; 
-    $db_server = "localhost:8111";
+    $db_server = "localhost:8011";
     $username = "root";
     $password = "";
-    $db_name = "faceboob";
+    $db_name = "facebook";
 
     $conn = new mysqli($db_server, $username, $password, $db_name);
-
+    
     if ($conn->connect_error) {
       die("Error: " . $conn->connect_error);
     }
